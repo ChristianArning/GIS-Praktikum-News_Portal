c98cbe58109cbbdcd554a613b825c29ce3baa15b
ChristianArning <101625241+ChristianArning@users.noreply.github.com>
2022-05-21 23:03:00 +0200

    Changed position of category selection

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -220,28 +220,22 @@ let selElmnt;
 let a;
 let b;
 let c;
-/* Look for any elements with the class "custom-select": */
+
 x = document.getElementsByClassName("custom-select");
 l = x.length;
 for (i = 0; i < l; i++) {
     selElmnt = x[i].getElementsByTagName("select")[0];
     ll = selElmnt.length;
-    /* For each element, create a new DIV that will act as the selected item: */
     a = document.createElement("DIV");
     a.setAttribute("class", "select-selected");
     a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
     x[i].appendChild(a);
-    /* For each element, create a new DIV that will contain the option list: */
     b = document.createElement("DIV");
     b.setAttribute("class", "select-items select-hide");
     for (j = 1; j < ll; j++) {
-        /* For each option in the original select element,
-        create a new DIV that will act as an option item: */
         c = document.createElement("DIV");
         c.innerHTML = selElmnt.options[j].innerHTML;
         c.addEventListener("click", function(e) {
-            /* When an item is clicked, update the original select box,
-            and the selected item: */
             let y;
             let i;
             let k;
@@ -271,8 +265,6 @@ for (i = 0; i < l; i++) {
     }
     x[i].appendChild(b);
     a.addEventListener("click", function(e) {
-        /* When the select box is clicked, close any other select boxes,
-        and open/close the current select box: */
         e.stopPropagation();
         closeAllSelect(this);
         this.nextSibling.classList.toggle("select-hide");
@@ -281,8 +273,6 @@ for (i = 0; i < l; i++) {
 }
 
 function closeAllSelect(elmnt) {
-    /* A function that will close all select boxes in the document,
-    except the current select box: */
     let x;
     let y;
     let i;
@@ -307,6 +297,4 @@ function closeAllSelect(elmnt) {
     }
 }
 
-/* If the user clicks anywhere outside the select box,
-then close all select boxes: */
 document.addEventListener("click", closeAllSelect);
